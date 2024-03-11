efda64c1d1989d5757a4130f2fbb1c8aed3aa4b6
Raul Igna <ignaraul12@yahoo.com>
2024-03-11 16:30:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ivi.Driver.Interop;
+using Agilent.AgAPS.Interop;
+
+namespace TestRegenerativePowerSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AgAPS driver = new AgAPS();
+
+            driver.Initialize("USB0::0x2A8D::0x2802::MY63000137::0::INSTR", false, false, "");
+            Console.WriteLine(driver.Initialized ? "Deschis" : "Nu");
+            driver.Close();
+
+
+        }
+    }
+}
